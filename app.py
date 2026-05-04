3ea66c8dfcbeadedc86b19786a866a77d4020502
maswin18 <280404051+maswin18@users.noreply.github.com>
2026-05-04 12:18:20 -0400

    add get single ticket endpoint with 404 handling

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import models
 import schemas
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from database import engine, Base, get_db
 from sqlalchemy.orm import Session
 
@@ -39,3 +39,12 @@ def create_ticket(ticket: schemas.TicketCreate, db: Session = Depends(get_db)):
 def get_tickets(db: Session = Depends(get_db)):
     tickets = db.query(models.Ticket).all()
     return tickets
+
+@app.get("/tickets/{ticket_id}", response_model=schemas.TicketResponse)
+def get_ticket(ticket_id: int, db: Session = Depends(get_db)):
+    ticket = db.query(models.Ticket).filter(models.Ticket.id == ticket_id).first()
+
+    if not ticket:
+        raise HTTPException(status_code=404, detail="Ticket not found")
+
+    return ticket
